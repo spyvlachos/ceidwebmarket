a0425544bccf1fec2fe1d333ab11e9a9b3916c9d
up1067540 <51995737+FayAgg@users.noreply.github.com>
2022-12-27 17:53:37 +0200

    Update and rename score.js to scoreToken.js

---
+++ model/scoreToken.js
@@ -0,0 +1,32 @@
+
+const mongoose = require("mongoose");
+
+const scoreTokenSchema = new mongoose.Schema({
+id: {
+    type : String
+}    
+userId: {
+    type: String
+},                                   
+scoreValue: {
+    type: Number,
+    default: "0"
+},
+
+ tokenValue: {
+     type: Number,
+       default: "0"
+    },
+  dateOfToken:{
+      type: Date(),
+      default: now()
+  }
+        
+},
+
+    },{ collection: 'scores' }
+ )
+
+
+const scoreTokens = mongoose.model('scoreToken', scoreTokensSchema);
+module.exports = scoreTokens;
