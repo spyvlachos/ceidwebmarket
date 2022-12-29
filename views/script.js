9a5e091114849d429738584e6190bc3f45e5dc8e
spyvlachos <81364057+spyvlachos@users.noreply.github.com>
2022-12-29 13:05:51 +0200

    Add files via upload

---
+++ views/script.js
@@ -0,0 +1,37 @@
+//const { query } = require("express");
+
+//let search = $("#livesearch");
+//function showResults(str) {
+  //  if (str.length === 0) {
+    //    search.addClass("hide");
+//
+  //  } else search.removeClass("hide")
+//
+  ////  $.ajax({
+      //  url: "/",
+        //contentType: "application/json",
+       // method: "POST",
+        //data: JSON.sringify({ query: str }),
+   //     success: function (result) {
+     //       search.html(result.result);
+       // }
+
+  //  })
+//}
+/*
+function showstore(str) {
+    if (str !== "") {
+        $.ajax({
+            url: "/",
+        contentType: "application/json",
+        method: "POST",
+        data: JSON.sringify({ storename: str }),
+      success: function (result) {
+         
+          $("#txt-hint").html(result.html);
+       }
+
+        })
+    }
+}
+*/
